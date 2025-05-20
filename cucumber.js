b9bbbd92088b862e61c4e1473d92c6ef57ba35a6
CarlossViniciuss <carlosvinicius.index@gmail.com>
2025-05-20 16:14:04 -0300

    Subida dos arquivos que se relacionam com o commit anterior

---
+++ cucumber.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    default: {
+        requireModule: ['ts-node/register'],
+        require: [
+            'tests/features/support/world.ts',
+            'tests/features/support/hooks.ts',
+            'tests/features/step_definitions/**/*.ts'
+        ],
+        paths: ['tests/features/**/*.feature'],
+        format: ['@cucumber/pretty-formatter'],
+        language: 'pt'
+    }
+}
