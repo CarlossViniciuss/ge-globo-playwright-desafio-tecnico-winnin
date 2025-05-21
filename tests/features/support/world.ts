3d8251218a4db37604cbc67281703fca17d8ad63
CarlossViniciuss <carlosvinicius.index@gmail.com>
2025-05-21 17:01:41 -0300

    ci: Adicionei o flow do github actions, e removi mensagem de lembrete para remover o handless do world quando for implementar o CI.

--- tests/features/support/world.ts
+++ tests/features/support/world.ts
@@ -18,7 +18,7 @@ export class CustomWorld extends World {
   }
 
   async openBrowser() {
-    this.browser = await chromium.launch({ headless: false }); // troque pra true se for CI
+    this.browser = await chromium.launch({ headless: process.env.CI ? true : false })
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
 
